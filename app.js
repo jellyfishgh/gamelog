58aafa6d3476e1d3c4934981023809282a6c71bd
Jellyfish <1129088994@qq.com>
2016-08-04 13:45:42 +0800

    win10:bugfix

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const handler = require('./handler');
 const config = require('./config.js');
 
 let port = config.server.port,
-	host = config.server.host;
+    host = config.server.host;
 
 http.createServer((req, res) => {
-	let urlObj = url.parse(req.url);
-	let queryObj = query.parse(urlObj.query);
-	if (config.logurls[queryObj.game]) handler.handle(queryObj.game, req, res);
-	else {
-		let body = 'no such game log';
-		res.writeHead(404, {
-			'Content-Type': 'text/plain',
-			'Content-Length': Buffer.byteLength(body);
-		});
-		res.end(body);
-	}
+    let urlObj = url.parse(req.url);
+    let queryObj = query.parse(urlObj.query);
+    if (config.logurls[queryObj.game]) handler.handle(queryObj.game, req, res);
+    else {
+        let body = 'no such game log';
+        res.writeHead(404, {
+            'Content-Type': 'text/plain',
+            'Content-Length': Buffer.byteLength(body)
+        });
+        res.end(body);
+    }
 }).listen(port, host, (err) => {
-	if (err) throw err;
-	console.log(`server running at ${host}:${port}`);
+    if (err) throw err;
+    console.log(`server running at ${host}:${port}`);
 });
